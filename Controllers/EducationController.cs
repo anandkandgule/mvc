eda6c731dea4386c02b4fa12c6cc8b45d0292f30
sajanand <sajanandkandgule67@gmail.com>
2022-11-28 15:54:41 +0530

    mvc

---
+++ Controllers/EducationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sajanand.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Experince()
+        {
+            return View();
+        }
+        public ActionResult Hobby()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sajanand.MvcApplication" Language="C#" %>
